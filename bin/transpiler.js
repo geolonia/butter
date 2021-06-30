f87590b201fcd0e67890af7228dc44142e8d58a3
kamataryo <kamataryo@users.noreply.github.com>
2021-06-30 09:05:10 +0900

    Separate YML2JSON transpiler as a class

---
+++ bin/transpiler.js
@@ -0,0 +1,48 @@
+const jsYaml = require("js-yaml");
+
+/**
+ * noop walker to traverse and transform values.
+ * @param {string} key
+ * @param {any} value
+ * @param {object} parent
+ * @returns
+ */
+const defaultWalker = (key, value, parent) => value;
+
+/**
+ * Plugable YAML -> JSON Transpiler
+ */
+class Transpiler {
+  constructor(yaml, walk = defaultWalker) {
+    this._json = null;
+    this._object = null;
+    this._yaml = yaml;
+    this._walk = walk;
+  }
+  parse() {
+    this._object = jsYaml.load(this._yaml);
+    return this._object;
+  }
+
+  traverse(target = this._object) {
+    for (var key in target) {
+      if (target.hasOwnProperty(key)) {
+        const value = this._walk(key, target[key], target);
+        if (typeof value === "object") {
+          return this.traverse(value);
+        }
+      }
+    }
+    return target;
+  }
+
+  generate() {
+    this._json = JSON.stringify(this._object, null, 2);
+    return this._json;
+  }
+  toJSON() {
+    return this._json;
+  }
+}
+
+module.exports.Transpiler = Transpiler;
--- bin/watch.js
+++ bin/watch.js
@@ -1,25 +1,40 @@
-const chokidar = require("chokidar");
 const path = require("path");
 const fs = require("fs");
-const yaml = require("js-yaml");
+const chokidar = require("chokidar");
+const chalk = require("chalk");
+const { Transpiler } = require("./transpiler");
+
+const NOTICE = () => `[${chalk.cyan("notice")} ${new Date().toISOString()}]`;
+const WARN = () => `[${chalk.yellow("warn")} ${new Date().toISOString()}]`;
+const notice = (message) => process.stdout.write(`${NOTICE()} ${message}\n`);
+const warn = (message) => process.stdout.write(`${WARN()} ${message}\n`);
 
 const ymlPath = path.resolve(__dirname, "..", "style.yml");
 const jsonPath = path.resolve(__dirname, "..", "style.json");
 
-chokidar.watch(ymlPath).on("all", (event, path) => {
-  console.log({ event, path });
-  switch (event) {
-    case "add":
-    case "change":
-      const jsonObject = yaml.load(fs.readFileSync(ymlPath, "utf-8"));
-      try {
-        const json = JSON.stringify(jsonObject, null, 2);
-        fs.writeFileSync(jsonPath, json);
-      } catch (error) {
-        console.error(error);
-      }
-      break;
-    default:
-      break;
+const watcher = chokidar.watch(ymlPath);
+const walk = (key, value, parent) => {
+  if (key.startsWith("__")) {
+    delete parent[key];
+  } else {
+    return value;
   }
-});
+};
+
+watcher
+  .on("add", (path) => notice(`${chalk.green(path)} has been added.`))
+  .on("change", (path) => {
+    try {
+      const yaml = fs.readFileSync(ymlPath, "utf-8");
+      const transpiler = new Transpiler(yaml, walk);
+      transpiler.parse();
+      transpiler.traverse();
+      transpiler.generate();
+      const json = transpiler.toJSON();
+      fs.writeFileSync(jsonPath, json);
+      notice(`${chalk.green(path)} -> ${chalk.green(jsonPath)}.`);
+    } catch (error) {
+      warn(`Failed to convert ${path}.`);
+      warn(error);
+    }
+  });
